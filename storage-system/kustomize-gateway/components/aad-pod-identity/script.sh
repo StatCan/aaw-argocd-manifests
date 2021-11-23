cb7b94badb3ac2151af8542d6c5b2c26d91fa7ab
blair drummond <blair.drummond@statcan.gc.ca>
2021-11-23 12:02:08 -0500

    fix(fdi-gateway): use different base image

--- storage-system/kustomize-gateway/components/aad-pod-identity/script.sh
+++ storage-system/kustomize-gateway/components/aad-pod-identity/script.sh
@@ -2,22 +2,13 @@
 set -euxo pipefail
 cd /tmp
 
-retries=18
-while ((retries > 0)); do
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" || true
-    [ -f kubectl ] && break
-    echo "No connectivity; wait 60 seconds and retry"
-    sleep 10
-    ((retries --))
-done
-[ -f kubectl ] || exit 1
-
-chmod +x ./kubectl
+echo "Waiting 60 seconds..."
+sleep 60
 
 # login into az cli as pod identity
 az login --identity
 
-AZURE_STORAGE_KEY=$(./kubectl get secret azure-blob-storage -n $NAMESPACE -o jsonpath="{.data.storageAccountKey}" | base64 -d)
+AZURE_STORAGE_KEY=$(kubectl get secret azure-blob-storage -n $NAMESPACE -o jsonpath="{.data.storageAccountKey}" | base64 -d)
 
 # get storage account key every 60 seconds. if just retrieved key doesnt match current key
 # patch secret that holds storage account creds then restart minio-gateway deployment
@@ -28,8 +19,8 @@ while true; do
         exit 1;
     fi
     if [ "$AZURE_STORAGE_KEY" != "$NEW_KEY" ]; then
-        ./kubectl patch secret azure-blob-storage -n $NAMESPACE -p="{\"data\":{\"storageAccountKey\": \"$NEW_KEY\"}}"
-        ./kubectl rollout restart deployment minio-gateway
+        kubectl patch secret azure-blob-storage -n $NAMESPACE -p="{\"data\":{\"storageAccountKey\": \"$NEW_KEY\"}}"
+        kubectl rollout restart deployment minio-gateway
         AZURE_STORAGE_KEY=$NEW_KEY
     fi
     sleep 60
